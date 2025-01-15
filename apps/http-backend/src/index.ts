cd25a0679d381730cd3baaf4b451fc1b7694d2cd
Fahad <fahad.khan2216@gmail.com>
2025-01-15 16:41:35 +0530

    basic setup

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express"
+
+const app = express();
+
+app.post("/signup", (req, res) => {
+    
+})
+app.post("/signin", (req, res) => {
+
+})
+app.post("/room", (req, res) => {
+
+})
+
+app.listen(3000);
